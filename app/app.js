6a89bc3163f27c010eccda82172b074ae9e045fb
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2023-12-31 11:51:12 +0100

    implemented endpoints & first aggregation query

--- app/app.js
+++ app/app.js
@@ -48,6 +48,7 @@ function check(req, res, next) {
 }
 
 app.get('/api/budget/', check, async (req, res) => {
+    console.log(`/api/budget/`);
     const client = new MongoClient(uri);
     await client.connect();
     const database = client.db("familybudget");
@@ -57,20 +58,33 @@ app.get('/api/budget/', check, async (req, res) => {
 });
 
 app.get('/api/budget/:year', check, async (req, res) => {
+    console.log(`/api/budget/:year`);
     const client = new MongoClient(uri);
     await client.connect();
     const database = client.db("familybudget");
-    const budget = await database.collection("expenses").find({ "user": req.session.username, "date": { "$gte": new Date("2023-01-01"), "$lt": new Date("2024-01-01") } }).toArray();
+    const budget = await database.collection("expenses").find({ "user": req.session.username, "date": { "$gte": new Date('2023-01-01'), "$lt": new Date("2024-01-01") } }).toArray();
     res.json(budget);
 
 });
 
+app.get('/api/budget/search/:query', check, async (req, res) => { //*** 
+    const client = new MongoClient(uri);
+    await client.connect();
+    const database = client.db("familybudget");
+    console.log(`/api/budget/search/:query`);
+    const results = await database.collection("expenses").find({ "description": { "$regex": req.params.query, "$options": "i" } }).toArray();
+    res.json(results);
+
+});
+
 app.get('/api/budget/:year/:month', check, (req, res) => {
+    console.log(`/api/budget/:year/:month`);
     res.json(req.session.user);
 
 });
 
 app.get('/api/budget/:year/:month/:id', check, (req, res) => {
+    console.log(`/api/budget/:year/:month/:id`);
     res.json(req.session.user);
 
 });
@@ -85,22 +99,48 @@ app.put('/api/budget/:year/:month/:id', check, (req, res) => {
 
 });
 
-app.delete('/api/budget/:year/:month/:id', check, (req, res) => {
+app.delete('/api/budget/id', check, async (req, res) => { //aggiungere year e month
+    console.log(`/api/budget/id`);
+    const client = new MongoClient(uri);
+    await client.connect();
+    const database = client.db("familybudget");
+    const result = await database.collection("expenses").deleteOne({ "_id": req.body.id });
     res.json(req.session.user);
+
 
 });
 
-app.get('/api/balance', check, (req, res) => {
-    res.json(req.session.user);
+app.get('/api/balance', check, async (req, res) => {
+    console.log(`/api/balance`);
+    const client = new MongoClient(uri);
+    await client.connect();
+    const database = client.db("familybudget");
+    const result = await database.collection("expenses").aggregate([ //si può semplificare?
+        {
+            '$match': {
+                'user': 'marcolg'
+            }
+        }, {
+            '$group': {
+                '_id': null,
+                'total': {
+                    '$sum': '$price'
+                }
+            }
+        }, {
+            '$project': {
+                '_id': 0,
+                'total': 1
+            }
+        }
+    ]).toArray();
+
+    res.json(result); //devo restituire il risultato in json o solo il numero?
 
 });
 
 app.get('/api/balance/:id', check, (req, res) => {
-    res.json(req.session.user);
-
-});
-
-app.get('/api/budget/search?q=query', check, (req, res) => {
+    console.log(`/api/balance/:id`);
     res.json(req.session.user);
 
 });
@@ -110,7 +150,13 @@ app.get('/api/budget/whoami', check, (req, res) => {
 
 });
 
-app.get('/api/users/search?q=query', check, (req, res) => {
-    res.json(req.session.user);
+app.get('/api/users/search/:query', check, async (req, res) => { //eliminare password
+    const client = new MongoClient(uri);
+    await client.connect();
+    const database = client.db("familybudget");
+    console.log(`/api/users/search/:query`);
+    console.log(req.params.query);
+    const results = await database.collection("users").find({ "username": { "$regex": req.params.query, "$options": "i" } }).toArray();
+    res.json(results);
 
 });
