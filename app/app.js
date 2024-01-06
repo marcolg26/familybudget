c2ec619770bfdb4336478b3087e4fa203659444d
marcolg26 <62360013+marcolg26@users.noreply.github.com>
2024-01-06 22:52:15 +0100

    new db url + script scr reorder + refactoring

--- app/app.js
+++ app/app.js
@@ -5,7 +5,8 @@ const app = express();
 const port = 3000;
 
 const { MongoClient, ObjectId } = require("mongodb");
-const uri = "mongodb://127.0.0.1:27017";
+//const uri = "mongodb://127.0.0.1:27017";
+const uri = "mongodb://mongohost";
 
 const client = new MongoClient(uri);
 
